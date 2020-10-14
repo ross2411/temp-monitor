fe2bf6a0b3bb0cde657356f2a111a35172bf5e5d
Ross Ellerington <ross.ellerington@tredsoft.co.uk>
2020-10-14 21:50:49 +0100

    Working (now I'm deplhing the right thing!)

--- TempMonitor/Server/Controllers/WeatherForecastController.cs
+++ TempMonitor/Server/Controllers/WeatherForecastController.cs
@@ -20,11 +20,11 @@ namespace TempMonitor.Server.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> logger;
+        private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            this.logger = logger;
+            this._logger = logger;
         }
 
         [HttpGet]
@@ -41,36 +41,70 @@ namespace TempMonitor.Server.Controllers
         }
 
         [HttpGet("GetFiles")]
-        public IActionResult GetFiles([FromQuery]int period)
+        public IActionResult GetFiles([FromQuery]int period, [FromQuery] DateTime? date)
         {
-            logger.LogInformation("Period: {0}", period);
-            var files = GetFilePaths(period);
+            _logger.LogInformation("New entry in logs");
+            _logger.LogInformation("Period: {0}. Date: {1}", period, date);
+            var files = GetFilePaths(period, date);
             var temperatures = new List<Temperature>();
             foreach(var file in files)
             {
-                logger.LogInformation("Extracting data from {0}", file);
+                _logger.LogInformation("Extracting data from {0}", file);
                 if (System.IO.File.Exists(file))
                 {
-                    using CsvReader reader = new CsvReader(new StreamReader(file), new CsvConfiguration(CultureInfo.CurrentCulture)
+                    using CsvReader reader = new CsvReader(new StreamReader(file), new CsvConfiguration(CultureInfo.GetCultureInfo("en-GB"))
                     {
-                        HasHeaderRecord = false
+                        HasHeaderRecord = false,
+                        
                     }
                     );
-                    temperatures.AddRange(reader.GetRecords<Temperature>());
+                    var temps = reader.GetRecords<Temperature>();
+                    var lessGranular = Granularity(temps, 300);
+                    temperatures.AddRange(lessGranular);
+                }
+                else
+                {
+                    _logger.LogError("Unable to find file {0}", file);
                 }
             }
             
             return this.Ok(temperatures);
         }
 
-        private IEnumerable<string> GetFilePaths(int period)
+        private IEnumerable<Temperature> Granularity(IEnumerable<Temperature> temps, int granularity)
         {
+            List<Temperature> toReturn = new List<Temperature>();
+            DateTime? lastPeriod = null;
+            foreach (var t in temps)
+            {
+                if (lastPeriod == null)
+                {
+                    lastPeriod = t.dateTime;
+                    toReturn.Add(t);
+                }
+                else
+                {
+                    if (t.dateTime >= lastPeriod.Value.AddSeconds(granularity))
+                    {
+                        toReturn.Add(t);
+                        lastPeriod = t.dateTime;
+                    }
+                }
+            }
+            return toReturn;
+        }
+
+        private IEnumerable<string> GetFilePaths(int period, DateTime? date = null)
+        {
+            if (date == null)
+                date = DateTime.Now;
+
             var basePath = "/var/temps";
-            var today = DateTime.Now;
+            //var basePath = "/Users/rossellerington/Projects/TempMonitor/TempMonitor/Server/Data";
             var fp = new List<string>();
             for( int i=period; i> 0; i--)
             {
-                fp.Add( $"{basePath}/{today.AddDays(-1 * i):dd-MM-yy}_temps.csv");
+                fp.Add( $"{basePath}/{date.Value.AddDays(-1 * i):dd-MM-yy}_temps.csv");
             }
             return fp;
 
--- TempMonitor/Shared/Temperature.cs
+++ TempMonitor/Shared/Temperature.cs
@@ -1,16 +1,53 @@
 ﻿using System;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
+using CsvHelper.TypeConversion;
+using Microsoft.Extensions.Logging;
+
 namespace TempMonitor.Shared
 {
     public class Temperature
     {
         public DateTime Date => dateTime;
         [Index(0)]
+        [TypeConverter(typeof(CustomDateTimeConverter))]
         public DateTime dateTime { get; set; }
         [Index(1)]
         public double InsideTemp { get; set; }
         [Index(2)]
         public double Humidity { get; set; }
+    }
+
+    public  class CustomDateTimeConverter: TypeConverter
+    {
+        //private readonly ILogger<CustomDateTimeConverter> _logger;
+        //public CustomDateTimeConverter(
+        //    ILogger<CustomDateTimeConverter> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            //_logger.LogInformation("About to parse{0)", text);
+            if (DateTime.TryParseExact(text, "d / M / y HH:m:s", CultureInfo.InvariantCulture, DateTimeStyles.AllowWhiteSpaces, out var dt))
+            {
+                return dt;
+            }
+            else if (DateTime.TryParseExact(text, "d/M/y HH:m:s", CultureInfo.InvariantCulture, DateTimeStyles.AllowWhiteSpaces, out var dt2))
+            {
+                return dt2;
+            }
+            //DateTime.Parse(text, )
+            ///"Unable to parse date: 11/10/20 13:00:10"
+            throw new Exception($"Unable to parse date: {text}");
+            //var dt = DateTime.ParseExact(text, "d/M/y h:m:s", CultureInfo.InvariantCulture);
+            //return dt;
+            //return base.ConvertFromString(text, row, memberMapData);
+        }
+
     }
 
     public class Period
