b0398185c9b80a973f4945b449f0dc3bc3c9a9ac
Ross Ellerington <ross.ellerington@tredsoft.co.uk>
2020-11-03 22:45:18 +0000

    Versioninfo (#15)
    * Display versions
    
    * Versioning isn't working
    
    * Added versioning
    
    * Updated docker action

---
+++ TempMonitor/Server/Controllers/StatusController.cs
@@ -0,0 +1,29 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using TempMonitor.Shared;
+
+namespace TempMonitor.Server.Controllers
+{
+    public class StatusController : Controller
+    {
+        // GET
+        [HttpGet("VersionInfo")]
+        public IActionResult Version()
+        {
+            // var version = Assembly.GetExecutingAssembly()
+            //         .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false)[0]
+            //     .InformationalVersion;
+            // var version = ((AssemblyInformationalVersionAttribute)Assembly
+            //         .GetExecutingAssembly()
+            //         .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false)[0])
+            //     .InformationalVersion;
+            var version = Assembly.GetExecutingAssembly()
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion;
+            return Ok(new VersionInfo
+            {
+                AssemblyVersion = version
+            });
+        }
+    }
+}
